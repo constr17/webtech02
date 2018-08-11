2ab1ce866594f331be60b0c481cbe4a6af6b6828
constr17 <bserge@bk.ru>
2018-08-11 14:25:42 +0300

    Сделал несколько маленьких тестов

--- project-test/test/webtech2-test.js
+++ project-test/test/webtech2-test.js
@@ -4,13 +4,20 @@ const index = require("../index")
 describe("Basic test", function() {
     it("Man basic", function() {
         assert(index.man, "index.man exists")
+    })
+    it("Man basic", function() {
         let man = index.man
         assert(man.name, "man.name exists")
+    })
+    it("Man basic", function() {
+        let man = index.man
         assert(man.surname, "man.surname exists")
+    })
+    it("Man basic", function() {
+        let man = index.man
         assert(man.fullName() == `${man.surname} ${man.name}`, "man.fullName() returns surname + ' ' + name")
     })
     it("Man basic", function() {
-        assert(index.man, "Man exists")
         let man = index.man
         man.name = "serge"
         man.surname = "belyaev"
@@ -18,8 +25,13 @@ describe("Basic test", function() {
     })
     it("Class basic", function() {
         assert(index.Room, "index.Room exists")
+    })
+    it("Class basic", function() {
         let room = new index.Room()
         assert(room.windows > 0, "room.windows is number and more than 0")
+    })
+    it("Class basic", function() {
+        let room = new index.Room()
         assert(room.owner, "room.owner exists")
     })
 })
