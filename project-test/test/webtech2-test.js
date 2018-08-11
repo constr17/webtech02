02dc5ce304334692167a4a96c9aa988abaeac9f8
constr17 <bserge@bk.ru>
2018-08-11 14:31:01 +0300

    Изменил описания тестов

--- project-test/test/webtech2-test.js
+++ project-test/test/webtech2-test.js
@@ -2,35 +2,35 @@ const assert = require("assert")
 const index = require("../index")
 
 describe("Basic test", function() {
-    it("Man basic", function() {
+    it("man", function() {
         assert(index.man, "index.man exists")
     })
-    it("Man basic", function() {
+    it("name", function() {
         let man = index.man
         assert(man.name, "man.name exists")
     })
-    it("Man basic", function() {
+    it("surname", function() {
         let man = index.man
         assert(man.surname, "man.surname exists")
     })
-    it("Man basic", function() {
+    it("fullName", function() {
         let man = index.man
         assert(man.fullName() == `${man.surname} ${man.name}`, "man.fullName() returns surname + ' ' + name")
     })
-    it("Man basic", function() {
+    it("fullName extended", function() {
         let man = index.man
         man.name = "serge"
         man.surname = "belyaev"
         assert(man.fullName() == `${man.surname} ${man.name}`, "Fullname function works correctly")
     })
-    it("Class basic", function() {
+    it("Room", function() {
         assert(index.Room, "index.Room exists")
     })
-    it("Class basic", function() {
+    it("windows", function() {
         let room = new index.Room()
         assert(room.windows > 0, "room.windows is number and more than 0")
     })
-    it("Class basic", function() {
+    it("owner", function() {
         let room = new index.Room()
         assert(room.owner, "room.owner exists")
     })
